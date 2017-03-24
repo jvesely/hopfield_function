d39ca430869e950fd58cb96277c9238eac5877cc
Jan Vesely <jan.vesely@rutgers.edu>
2017-03-24 13:10:53 -0400

    hopfield_func: Add option to pad function result with random data
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- hopfield_func.py
+++ hopfield_func.py
@@ -4,13 +4,14 @@ import numpy as np
 from hopfield_network import HopfieldNetwork
 
 class HopfieldFunc(HopfieldNetwork):
-    def __init__(self, domain_bits, range_bits, iterations = 100):
+    def __init__(self, domain_bits, range_bits, iterations = 100, pad_zeros = False):
         assert domain_bits > 0 and domain_bits < 48
         assert range_bits > 0 and range_bits < 48
         self.__domain_bits = domain_bits
         self.__range_bits = range_bits
         #using super() does not work for some reason
         self.__network = HopfieldNetwork(domain_bits + range_bits, iterations)
+        self.__pad_zeros = pad_zeros
 
     def set(self, x, y):
         bin_x = np.array([int(x) for x in bin(x)[2:]])
@@ -22,10 +23,9 @@ class HopfieldFunc(HopfieldNetwork):
 
     def get(self, x):
         bin_x = np.array([int(x) for x in bin(x)[2:]])
+        padding = n.zeros(self.__range_bits) if self.__pad_zeros else np.random.randint(0, 1, self.__range_bits)
         memory = self.__network.recall(np. concatenate(( \
-                    np.zeros(self.__domain_bits - bin_x.size), bin_x, \
-                    np.zeros(self.__range_bits))))
-#        print(memory)
+                    np.zeros(self.__domain_bits - bin_x.size), bin_x, padding)))
         bin_res = memory[self.__range_bits:]
         return int(''.join(str(int(x)) for x in bin_res), 2)
 
