bc3fa300cb6b11951766919665bc2a0c2ca6ad7e
Jan Vesely <jan.vesely@rutgers.edu>
2017-04-11 13:06:13 -0400

    hopfield_func: print warning when the network changes input neurons
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- hopfield_func.py
+++ hopfield_func.py
@@ -1,7 +1,11 @@
 #!/bin/env python3
 
+import sys
 import numpy as np
 from hopfield_network import HopfieldNetwork
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 class HopfieldFunc(HopfieldNetwork):
     def __init__(self, domain_bits, range_bits, iterations = 100, pad_random = True):
@@ -28,7 +32,14 @@ class HopfieldFunc(HopfieldNetwork):
                     np.zeros(self.__domain_bits - bin_x.size), bin_x, padding)))
         bin_res = memory[self.__domain_bits:]
         assert(bin_res.size == self.__range_bits)
-        return int(''.join(str(int(x)) for x in bin_res), 2)
+        int_res = int(''.join(str(int(x)) for x in bin_res), 2)
+
+        bin_x = memory[:self.__domain_bits]
+        assert(bin_x.size == self.__domain_bits)
+        int_x = int(''.join(str(int(x)) for x in bin_x), 2)
+        if (int_x != x):
+            eprint("Warning input changed: %x -> %x" % (x, int_x))
+        return int_res
 
     def __call__(self, x):
         return self.get(x);
