1f2ec1fbd4c499667c055f1733180c7a9778cf65
Jan Vesely <jan.vesely@rutgers.edu>
2017-04-11 13:58:35 -0400

    hopfield_network: Add Hoppfield original implementation variant
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- hopfield_network.py
+++ hopfield_network.py
@@ -5,11 +5,12 @@ import numpy as np
 class HopfieldNetwork(object):
     async_default = False
     iterations_default = 1000
-    def __init__(self, dim, iterations = iterations_default, async = async_default):
+    def __init__(self, dim, iterations = iterations_default, async = async_default, hoppfield_original = False):
         assert dim > 0
         self.__matrix = np.zeros((dim,dim))
         self.__iterations = iterations
         self.__async = async
+        self.__hoppfield_original = hoppfield_original;
 
     ''' the matrix format is:
         sum{p}{outer_product(memory_i, memory_i)} /N  - (p/N)(Identity)
@@ -20,11 +21,16 @@ class HopfieldNetwork(object):
     '''
     def remember(self, data):
         # convert '0' -> '-1'
-        data[data < 0.5] = -1
+        data[data <= 0] = -1
+
+        # The original Hoppfield paper does not normalize the weights
+        # to number of neurons
+        scale = 1 if self.__hoppfield_original else self.__matrix.size
 
         # create new memory
-        new_memory = np.asmatrix(np.outer(data, data)) / self.__matrix.size
+        new_memory = np.asmatrix(np.outer(data, data)) / scale
         np.fill_diagonal(new_memory, 0)
+
         assert new_memory.shape == self.__matrix.shape
 
         # add the new memory to memory matrix
@@ -35,7 +41,9 @@ class HopfieldNetwork(object):
 
         # get iterations overload
         iterations = self.__iterations if (iterations is None) else iterations
-        partial_data[partial_data < 0.5] = -1
+        # The original paper uses {0,1} for neuronal representation
+        if (not self.__hoppfield_original):
+            partial_data[partial_data < 0.5] = -1
        
         # turn partial data into column vector
         probe = np.asarray(partial_data) if self.__async else np.asmatrix(partial_data).transpose()
@@ -48,6 +56,9 @@ class HopfieldNetwork(object):
                     probe[v] = np.sign(np.dot(self.__matrix[v], probe))
             else:
                 probe = np.sign(np.matmul(self.__matrix, probe))
+            # The original paper uses {0,1} for neuronal states
+            if (self.__hoppfield_original):
+                probe[probe <= 0] = 0
 
         # turn column vector back into array
         res = np.squeeze(np.asarray(probe.transpose()))
