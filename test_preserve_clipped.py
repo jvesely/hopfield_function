db34ad8cd7323479cd4a9ff58ef77c871824b065
Jan Vesely <jan.vesely@rutgers.edu>
2017-05-13 21:12:40 -0400

    add clipped test script
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

---
+++ test_preserve_clipped.py
@@ -0,0 +1,35 @@
+#!/bin/env python3
+
+import numpy as np
+from hopfield_func import HopfieldFunc
+
+DOM_BITS = 36
+RNG_BITS = 34
+STORE = 20
+COUNT = 10
+
+hf = HopfieldFunc(DOM_BITS, RNG_BITS, preserve = True, storage = COUNT)
+
+def test(hf, a, b):
+    a %= 2 ** DOM_BITS
+    b %= 2 ** RNG_BITS
+#    print("f(%s) = %s" % (a, b))
+    hf.set(a, b)
+#    print(hf(a))
+
+
+keys = np.random.randint(0, 2 ** DOM_BITS, STORE)
+values = np.random.randint(0, 2 ** RNG_BITS, STORE)
+
+for x in map(lambda x,y: (x,y), keys, values):
+    test(hf, x[0], x[1])
+
+results = [(x[0], x[1], hf(x[0])) for x in map(lambda x,y: (x,y), keys[-COUNT:], values[-COUNT:])]
+
+res = sum(x[1] == x[2] for x in results)
+for x in results:
+    if x[2] != x[1]:
+        print("FAIL: hf(%x) = %x instead of %x" % (x[0], x[2], x[1]))
+    else:
+        print("PASS: hf(%x) = %x" % (x[0], x[1]))
+print("Successfully remembered: %d out of %d" % (res, COUNT))
